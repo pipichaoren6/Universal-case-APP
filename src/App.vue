525b42fceb1eca17c0292203e1e7091bd80b088f
chenluo <2695577569@qq.com>
2025-03-04 13:51:29 +0800

    合并冲突

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HelloWorld from './views/login.vue'
+</script>
+
+<template>
+   <router-view /> <!-- 使用 router-view 来显示路由组件 -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from './router/router' // 引入路由
+import VConsole from 'vconsole'; // 引入 vConsole
+
+// 初始化 vConsole
+const vConsole = new VConsole();
+ 
+createApp(App)
+    .use(router)
+    .mount('#app')
